6d2c409be3bde9f986c3307c2effc5e30155bc82
hhh <gks0405@koreatech.ac.kr>
2020-04-28 22:55:02 +0900

    0428_1

--- community/user/views.py
+++ community/user/views.py
@@ -9,18 +9,22 @@ def register(request):
         return render(request, 'register.html')
     elif request.method=='POST':
         username=request.POST.get('username',None)
+        email=request.POST.get('email',None)
         password=request.POST.get('password',None)
         re_password=request.POST.get('re-password',None)
+        
 
         res_data={}
-        if not (username and password and re_password):
+        if not (username and password and re_password and email):
             res_data['error']='모든 값을 입력해야합니다'
         elif password!=re_password:
             res_data['error']='비밀번호가 다릅니다.'
         else:
             User=user(
                 username=username,
+                email=email,
                 password=make_password(password)
+                
             )
             User.save()
         return render(request, 'register.html',res_data)
