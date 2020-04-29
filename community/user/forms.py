2a7884cd64e5d37e729f862580ed13c9a85fc99f
hyunhee han <gks0405@koreatech.ac.kr>
2020-04-29 16:07:20 +0900

    0429_form

---
+++ community/user/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=32,label='사용자 이름')
+    password=forms.CharField(widget=forms.PasswordInput,label='비밀번호')
--- community/user/views.py
+++ community/user/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import user
+from .forms import LoginForm
 
 # Create your views here.
 def home(request):
@@ -10,24 +11,15 @@ def home(request):
         User=user.objects.get(pk=user_id)
         return HttpResponse(User.username)
     return HttpResponse('Home!')
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+    return redirect('/')
 def login(request):
-    if request.method=='GET':
-        return render(request,'login.html')
-    elif request.method=='POST':
-        username=request.POST.get('username',None)
-        password=request.POST.get('password',None)
-        res_data={}
-        if not (username and password):
-            res_data['error']='모든 값을 입력해야합니다.'
-        else:
-            User=user.objects.get(username=username)
-            if check_password(password, User.password):
-                request.session['user'] = User.id
-                return redirect('/')
-            else:
-                res_data['error']='비밀번호가 틀렸습니다.'
+    form = LoginForm()
+    
 
-        return render(request,'login.html',res_data)
+    return render(request,'login.html',{'form': form})
 def register(request):
     if request.method=='GET':
         return render(request, 'register.html')
